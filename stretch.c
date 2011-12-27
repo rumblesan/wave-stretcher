c93ef19e4787482a3d706f6558e902d03a7a3f19
Guy John <d.guyjohn@gmail.com>
2011-12-27 07:22:27 -0500

    Adding more to stretch output functions

--- stretch.c
+++ stretch.c
@@ -60,6 +60,13 @@ void setup_stretch_output(struct stretch_output *s,
     s->window_size = window_size;
     s->offset = 0;
     s->output_size = 0;
+
+    s->output = (float*) malloc(sizeof(float)*(2*(s->window_size/2)));
+    int i;
+    for (i = 0; i < (2*(s->window_size/2)); i++) {
+        s->output[i] = 0;
+    }
+
 }
 
 void next_input_section (struct stretch_input *s) {
@@ -87,14 +94,22 @@ void next_input_section (struct stretch_input *s) {
 void add_output(struct stretch_output *o, float *lbuffer, float *rbuffer) {
 
     int i,j;
-    o->output = (float*) realloc(o->output, sizeof(float)*(o->output_size+(2*o->window_size)));
-    for (i = 0; i < o->window_size; i++) {
-        j = (2 * i) + (int)o->offset;
-        o->output[i]   = lbuffer[i];
-        o->output[i+1] = lbuffer[i];
+    /*
+       because the windows are being overlapped we extend the output buffer by one window size
+       should be increased by half a windowsize for each channel
+    */
+    o->output = (float*) realloc(o->output, sizeof(float)*(o->output_size+(2*(o->window_size/2))));
+    for (i = 0; i < (2*2*(o->window_size/2)); i++) {
+        j = i + o->offset + (o->window_size/2);
+        o->output[j] = 0;
     }
 
-
+    for (i = 0; i < o->window_size; i++) {
+        j = (2 * i) + (int)o->offset;
+        o->output[j]   += lbuffer[i];
+        o->output[j+1] += rbuffer[i];
+    }
+    o->offset += (o->window_size/2);
 }
 
 
