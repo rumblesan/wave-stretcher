0d2bc55b12fe2cda3a3f2ce61fd1d0077af2cf28
Guy John <d.guyjohn@gmail.com>
2011-12-28 15:10:13 -0500

    moving fft over to using single data buffer

--- fft.c
+++ fft.c
@@ -8,7 +8,7 @@ struct fft_data {
 
     float *window;
 
-    float *in, *out;
+    float *data;
     fftwf_plan forward, inverse;
 
 };
@@ -21,10 +21,9 @@ void setup_fft (struct fft_data *d, int N) {
     d->window = (float*) malloc(N);
 
 
-    d->in      = (float*) fftwf_malloc(sizeof(float) * N);
-    d->out     = (float*) fftwf_malloc(sizeof(float) * N);
-    d->forward = fftwf_plan_r2r_1d(N, d->in, d->out, FFTW_FORWARD,  FFTW_ESTIMATE);
-    d->inverse = fftwf_plan_r2r_1d(N, d->in, d->out, FFTW_BACKWARD, FFTW_ESTIMATE);
+    d->data    = (float*) fftwf_malloc(sizeof(float) * N);
+    d->forward = fftwf_plan_r2r_1d(N, d->data, d->data, FFTW_FORWARD,  FFTW_ESTIMATE);
+    d->inverse = fftwf_plan_r2r_1d(N, d->data, d->data, FFTW_BACKWARD, FFTW_ESTIMATE);
 
 
     int i;
@@ -48,8 +47,7 @@ void cleanup_fft (struct fft_data *d) {
     fftw_destroy_plan(d->forward);
     fftw_destroy_plan(d->inverse);
 
-    fftw_free(d->in);
-    fftw_free(d->out);
+    fftw_free(d->data);
 
 }
 */
