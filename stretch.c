92611c910af24432be2b6c74b06345991b7352ff
Guy John <d.guyjohn@gmail.com>
2011-12-28 14:04:48 -0500

    updating stretch code
    moving everything into single struct. updating setup and get next section functions. also beginning to add test function

--- stretch.c
+++ stretch.c
@@ -2,118 +2,113 @@
 #include <stdio.h>
 
 
-struct stretch_input {
+struct stretch_data {
 
-    float *input;
-    int input_size;
+    float *input_data;
+    int    input_size;
+    float  input_offset;
 
-    int window_size;
-    int ratio;
-    float offset;
+
+    float *output_data;
+    int    output_size;
+    int    output_offset;
 
     int finished;
 
-    float *lbuffer, *rbuffer;
+    float ratio;
 
-};
-
-
-struct stretch_output {
-
-    float *output;
-
-    int output_size;
+    int channels;
 
     int window_size;
-    int offset;
 
-};
-
-struct stretch_data {
-    struct stretch_input  in;
-    struct stretch_output out;
+    float *buffers[];
 };
 
 
 
-void setup_stretch_input (struct stretch_input *s,
-                          float *wavdata,
-                          int input_size,
-                          int window_size,
-                          float ratio) {
+void setup_stretch (struct stretch_data *s,
+                    float *wavdata,
+                    int input_size,
+                    int window_size,
+                    int channels,
+                    float ratio) {
 
-    s->input      = wavdata;
-    s->input_size = input_size;
+    s->window_size  = window_size;
+    s->channels     = channels;
+    s->ratio        = 0.0;
 
-    s->window_size = window_size;
-    s->ratio       = ratio;
-    s->offset      = 0.0;
+    s->finished     = 0;
 
-    s->finished    = 0;
+    s->input_data   = wavdata;
+    s->input_size   = input_size;
+    s->input_offset = 0.0;
 
-    s->lbuffer = (float*) malloc(sizeof(float) * s->window_size);
-    s->rbuffer = (float*) malloc(sizeof(float) * s->window_size);
-}
-
-void setup_stretch_output(struct stretch_output *s,
-                          int window_size) {
-    s->window_size = window_size;
-    s->offset = 0;
-    s->output_size = 0;
-
-    s->output = (float*) malloc(sizeof(float)*(2*(s->window_size/2)));
+    /*
+       creating initial output buffer
+       initial size needs to be number:
+       channels * (window_size / 2)
+    */
+    s->output_size   = s->channels * (s->window_size / 2);
+    s->output_data   = (float*) malloc(sizeof(float) * s->output_size);
+    s->output_offset = 0;
     int i;
-    for (i = 0; i < (2*(s->window_size/2)); i++) {
-        s->output[i] = 0;
+    for (i = s->output_offset; i < s->output_size; i++) {
+        s->output_data[i] = 0;
     }
 
+
+    /*
+       create buffers. one for each channel
+    */
+    for (i = 0; i < s->channels; i++) {
+        s->buffers[i] = (float*) malloc(sizeof(float) * s->window_size);
+    }
 }
 
-void next_input_section (struct stretch_input *s) {
+void next_input_section (struct stretch_data *s) {
 
     int i, j, k;
-    float left,right;
+    float data;
+
     for (i = 0; i < s->window_size; i++) {
-        j = (2 * i) + (int)s->offset;
-        k = j + 1;
-        if (k >= s->input_size) {
-            left  = 0;
-            right = 0;
-            s->finished = 1;
-        } else {
-            left  = s->input[j];
-            right = s->input[k];
+        j = (s->channels * i) + (int)s->input_offset;
+        for (k = 0; k < s->channels; k++) {
+            data  = s->input_data[j+k];
+            s->buffers[k][i] = data;
         }
-        s->lbuffer[i] = left;
-        s->rbuffer[i] = right;
     }
 
-    s->offset += s->ratio * (s->window_size * 0.5);
+    s->input_offset += s->ratio * (s->window_size * 0.5);
 }
 
-void add_output(struct stretch_output *o, float *lbuffer, float *rbuffer) {
+void add_output(struct stretch_data *s, float *buffers[]) {
 
-    int i,j;
+    int i, j, k, N;
+    float data;
+
     /*
-       because the windows are being overlapped we extend the output buffer by one window size
-       should be increased by half a windowsize for each channel
-    */
-    o->output = (float*) realloc(o->output, sizeof(float)*(o->output_size+(2*(o->window_size/2))));
-    for (i = 0; i < (2*2*(o->window_size/2)); i++) {
-        j = i + o->offset + (o->window_size/2);
-        o->output[j] = 0;
-    }
+       because the windows are being overlapped we extend the output
+       buffer by half a windowsize for each channel
+   */
 
-    for (i = 0; i < o->window_size; i++) {
-        j = (2 * i) + (int)o->offset;
-        o->output[j]   += lbuffer[i];
-        o->output[j+1] += rbuffer[i];
+    N = s->output_size + s->channels * (s->window_size / 2);
+    s->output_data = (float*) realloc(s->output_data, sizeof(float) * N);
+    for (i = s->output_size; i < N; i++) {
+        s->output_data[i] = 0;
     }
-    o->offset += (o->window_size/2);
+    s->output_size = N;
+
+    for (i = 0; i < s->window_size; i++) {
+        j = (s->channels * i) + (int)s->output_offset;
+        for (k = 0; k < s->channels; k++) {
+            data  = buffers[k][i];
+        }
+        s->output_data[j] += data;
+    }
+    s->output_offset += s->channels * (s->window_size / 2);
 }
 
 
-/*
 void print_next_section (struct stretch_data *s) {
 
     int i;
@@ -134,17 +129,28 @@ void print_data (struct stretch_data *s) {
 
 void main () {
 
+    int channels = 2;
+    int samples  = 128;
+    int datasize = channels * samples;
     float *wdata;
-    wdata = (float*) malloc(sizeof(float) * 128 * 2);
+    wdata = (float*) malloc(sizeof(float) * datasize);
 
-    int i;
-    for (i = 0; i < (2 * 128); i += 2) {
-        wdata[i]   = i;
-        wdata[i+1] = i + 1.0;
+    int i,j,k;
+    for (i = 0; i < samples; i++) {
+        j = (i * channels);
+        for (k = 0; k < channels; k++) {
+            wdata[j+k] = 1 + (k/10);
+        }
     }
 
+void setup_stretch (struct stretch_data *s,
+                    float *wavdata,
+                    int input_size,
+                    int window_size,
+                    int channels,
+                    float ratio) {
     struct stretch_data sdata;
-    setup_stretch(&sdata, wdata, 8, 256, 2, 1);
+    setup_stretch(&sdata, wdata, 256, 2, 1);
 
     get_next_section(&sdata);
     print_next_section(&sdata);
@@ -153,5 +159,4 @@ void main () {
     print_next_section(&sdata);
 
 }
-*/
 
