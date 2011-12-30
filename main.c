973bece5284c86f499d100b59977863cddb1e118
Guy John <d.guyjohn@gmail.com>
2011-12-30 17:45:48 +0000

    Initial go at putting everything together
    not yet working but this should basically be the flow of the final program

--- main.c
+++ main.c
@@ -10,10 +10,42 @@ void main () {
 
     read_wav(&af);
 
-    print_wav(&af);
+    int window_size = 1024;
+    float ratio = 0.2;
+    struct stretch_data sdata;
+    setup_stretch(&sdata,
+                  af.sound_data,
+                  af.info.frames,
+                  af.info.channels,
+                  window_size,
+                  ratio);
 
-    af.filename = "output.wav";
-    write_wav(&af);
+    struct fft_data fft;
+    setup_fft(&fft, window_size);
+
+    int i;
+    while (sdata.finished != 1) {
+        next_input_section(&sdata);
+        for (i = 0; i < af.info.channels; i++) {
+            get_data(&fft, sdata.buffers[i]);
+            samp_to_freq(&fft);
+            freq_to_samp(&fft);
+            return_data(&fft, sdata.buffers[i]);
+        }
+        add_output(&sdata);
+    }
+
+    struct audio_file of;
+    of.filename        = "output.wav";
+    of.sf              = af.sf;
+    of.info.samplerate = af.info.samplerate;
+    of.info.channels   = af.info.channels;
+    of.info.format     = af.info.format;
+
+    write_wav(&of);
+
+    cleanup_fft(&fft);
+    free_wav(&af);
 
 }
 
