82e86409fdb4d8aa522ce297821f5698a0e19c57
Guy John <d.guyjohn@gmail.com>
2012-01-05 13:30:54 -0500

    Updates to wavreader section
    added sample buffer type
    file data is now not read all in one go. file read function will return a sample_buffer struct with a specified number of frames, already split intto channels

--- wavreader.c
+++ wavreader.c
@@ -3,23 +3,57 @@
 #include "wavreader.h"
 
 
-AudioFile read_wav(char *filename) {
+AudioFile read_audio_file(char *filename) {
 
     AudioFile af = (AudioFile) malloc(sizeof(AudioFile_Data));
 
     af->filename = filename;
+    af->finished = 0;
 
     af->info.format = 0;
     af->sf = sf_open(af->filename, SFM_READ, &af->info);
 
-    af->sound_data = (float*) malloc(sizeof(float) * af->info.channels * (int)af->info.frames);
-    sf_readf_float(af->sf, af->sound_data, af->info.frames);
-
     return af;
+}
+
+Samples get_audio_data(AudioFile af, int size) {
+
+    int channels = af->info.channels;
+    int i,j;
+
+    Samples smps = (Samples) malloc(sizeof(Samples_Data));
+    smps->size = size;
+    smps->channels = channels;
+
+    smps->buffers = (float**) malloc(sizeof(float*) * channels);
+    for (i = 0; i < channels; i++) {
+        smps->buffers[i] = (float*) malloc(sizeof(float*) * size);
+    }
+
+    int buffer_size = size * channels;
+
+    float iobuffer[buffer_size];
+    int read_amount = sf_readf_float(af->sf, iobuffer, size);
+    if (read_amount < buffer_size) {
+        af->finished = 1;
+        for (i = read_amount; i < buffer_size; i++) {
+            iobuffer[i] = 0.0;
+        }
+    }
+
+    for (i = 0; i < channels; i++) {
+        for (j = 0; j < size; j++) {
+            smps->buffers[i][j] = iobuffer[i+j];
+        }
+    }
+
+    return smps;
+
 }
 
 void write_wav(AudioFile af) {
 
+    /*
     sf_count_t frame_num = af->info.frames;
     af->sf = sf_open(af->filename, SFM_WRITE, &af->info);
     if (af->sf == NULL) {
@@ -28,12 +62,20 @@ void write_wav(AudioFile af) {
         sf_writef_float(af->sf, af->sound_data, frame_num);
         sf_close(af->sf);
     }
+    */
 }
 
-void cleanup_wav(AudioFile af) {
-
-    free(af->sound_data);
+void cleanup_audio_file(AudioFile af) {
     sf_close(af->sf);
     free(af);
+}
+
+void cleanup_sample_buffer(Samples smps) {
+    int i;
+    for (i = 0; i < smps->channels; i++) {
+        free(smps->buffers[i]);
+    }
+    free(smps->buffers);
+    free(smps);
 }
 
--- wavreader.h
+++ wavreader.h
@@ -9,11 +9,23 @@ typedef struct audio_file {
     SNDFILE *sf;
     char *filename;
     SF_INFO info;
-    float *sound_data;
+    int finished;
 } AudioFile_Data;
 
-AudioFile read_wav(char *filename);
+typedef struct audio_samples *Samples;
+typedef struct audio_samples {
+
+    int size;
+    int channels;
+
+    float **buffers;
+
+} Samples_Data;
+
+AudioFile read_audio_file(char *filename);
+Samples get_audio_data(AudioFile af, int size);
 void write_wav(AudioFile af);
-void cleanup_wav(AudioFile af);
+void cleanup_audio_file(AudioFile af);
+void cleanup_sample_buffer(Samples smps);
 
 #endif
